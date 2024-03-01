4dfb0b3a46c6c9a0a527227c76ae7ac06d1cdd33
Julian-David-tun <8135@itescam.edu.mx>
2024-03-01 15:49:28 -0600

    Agregue mi objeto persona a Archivo main

--- Main.java
+++ Main.java
@@ -3,6 +3,11 @@ public class Main {
         System.out.println("Jenry Yovani Keb Pech 8347");
 	System.out.println("Sergio Efrain Chuc Keb 8378");
         System.out.println("Julian David Tun Ortiz");
+
+
+
+	Persona julian = new Persona("Julian",8135);
+	julian.mostrar();
     }
 }
 

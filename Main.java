33cbd5d7ab68d9c2aa9be5e7c6eb6132a836a100
Julian-David-tun <8135@itescam.edu.mx>
2024-03-01 15:11:51 -0600

    Se actualizo con mi nombre

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Jenry Yovani Keb Pech 8347");
 	System.out.println("Sergio Efrain Chuc Keb 8378");
+        System.out.println("Julian David Tun Ortiz");
     }
 }
 
